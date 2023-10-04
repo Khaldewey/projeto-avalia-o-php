9458c53dc195f71b09b5c8d67c901fd57bcff099
Israel Alves <108505958+Khaldewey@users.noreply.github.com>
2023-10-04 17:57:11 +0000

    mudança na view filtro-table

--- app/views/filtro-table.php
+++ app/views/filtro-table.php
@@ -1,12 +1,12 @@
 <div class="title-files">Filtro de Pagamentos</div>
-<table>
+<table style= "margin-left: 30%;">
     <thead>
         <tr>
             <th>ID</th>
             <th>Empresa</th>
             <th>Data de Pagamento</th>
             <th>Valor (R$)</th>
-            <th>Ações</th>
+            
         </tr>
     </thead>
     <tbody>
@@ -18,13 +18,7 @@
                 <td><?= $empresaNomes[$conta['id_empresa']]; ?></td>
                 <td><?= $conta['data_pagar']; ?></td>
                 <td>R$ <?= number_format($conta['valor'], 2, ',', '.'); ?></td>
-                <td>
-                    <a href="./app/controllers/editar_conta.php?id=<?= $conta['id_conta_pagar']; ?>" class="btn-editar">Editar</a>
-                    <a href="./app/controllers/excluir_conta.php?id=<?= $conta['id_conta_pagar']; ?>" class="btn-excluir">Excluir</a>
-                    <?php if (isset($conta['id_conta_pagar']) && !$conta['pago']): ?>
-                        <a href="./app/controllers/marcar_como_paga.php?id=<?= $conta['id_conta_pagar']; ?>" class="btn-pago">Marcar como Pago</a>
-                    <?php endif; ?>
-                </td>
+                
             </tr>
         <?php endforeach; ?>
     </tbody> 
